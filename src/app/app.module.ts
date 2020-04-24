25607b565b00ef1c6865036bab4f018c672e4092
Vimal Kumar S <vimalkmr250@gmail.com>
2020-04-24 07:46:24 +0530

    image Upload function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { MatExpansionModule } from '@angular/material/expansion';
 import{ MatButtonModule } from '@angular/material/button';
@@ -28,7 +28,7 @@ import { from } from 'rxjs';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,10 @@
 import { Component,OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { FormGroup, FormControl, Validator, Validators } from "@angular/forms";
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from "../posts.service";
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
 
 
 @Component ({
@@ -15,6 +16,9 @@ export class PostCreateComponent implements OnInit{
   enteredTitle = "";
   enteredContent = "";
   private mode = 'create';
+  form :FormGroup;
+
+  imagePreview :string;
   private postId:string;
   post:Post;
   isLoading = false;
@@ -22,6 +26,17 @@ export class PostCreateComponent implements OnInit{
   constructor(public postsService: PostsService,public route:ActivatedRoute) {}
 
   ngOnInit(){
+    this.form = new FormGroup({
+      'title': new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      'content': new FormControl(null , {
+         validators: [Validators.required, Validators.minLength(3)]
+      }),
+     'image': new FormControl(null , {
+        validators: [Validators.required,],asyncValidators:[mimeType]})
+    });
+
     this.route.paramMap.subscribe((paramMap:ParamMap)=>{
         if (paramMap.has("postId")){
           this.mode = "edit";
@@ -29,7 +44,16 @@ export class PostCreateComponent implements OnInit{
           this.isLoading = true;
           this.postsService.getPost(this.postId).subscribe(postData =>{
             this.isLoading= false;
-          this.post = {id: postData._id, title: postData.title , content : postData.content};
+          this.post = {
+            id: postData._id,
+             title: postData.title,
+              content : postData.content,
+               imagePath: postData.imagePath};
+          this.form.setValue({
+            'title': this.post.title,
+            'content': this.post.content,
+            'image' : this.post.imagePath
+          });
          });
         }else{
           this.mode="create";
@@ -38,17 +62,29 @@ export class PostCreateComponent implements OnInit{
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  onImagePicked(event : Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image:file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () =>{
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading= true;
     if(this.mode === "create"){
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     }
     else{
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+      this.postsService.updatePost(
+        this.postId, this.form.value.title, this.form.value.content, this.form.value.image);
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
